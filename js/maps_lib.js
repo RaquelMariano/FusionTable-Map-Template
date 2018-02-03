17fcef735e463fdf2f805a8256e63a59ac678e79
RaquelMariano <35544880+RaquelMariano@users.noreply.github.com>
2018-02-03 02:15:00 +0000

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -165,26 +165,26 @@
         //-----custom filters-----
         // NUMERICAL OPTION to filter checkboxes by numerical type
         // EDIT type_column and numbers to match your Google Fusion Table points AND index.html
-        var type_column = "'Icons'";
-        var searchType = type_column + " IN (-1,";
-        if ( $("#cbType1").is(':checked')) searchType += "1,";
-        if ( $("#cbType2").is(':checked')) searchType += "2,";
-        if ( $("#cbType3").is(':checked')) searchType += "3,";
-        if ( $("#cbType4").is(':checked')) searchType += "4,";
-        if ( $("#cbType5").is(':checked')) searchType += "5,";
-        if ( $("#cbType5").is(':checked')) searchType += "6,";
-        self.whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
+       // var type_column = "'Icons'";
+       // var searchType = type_column + " IN (-1,";
+       // if ( $("#cbType1").is(':checked')) searchType += "1,";
+       // if ( $("#cbType2").is(':checked')) searchType += "2,";
+       // if ( $("#cbType3").is(':checked')) searchType += "3,";
+       // if ( $("#cbType4").is(':checked')) searchType += "4,";
+       // if ( $("#cbType5").is(':checked')) searchType += "5,";
+       // if ( $("#cbType5").is(':checked')) searchType += "6,";
+       // self.whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
 
         // TEXTUAL OPTION to filter checkboxes by text type
         // EDIT type_column and EXACT words to match your Google Fusion Table points AND index.html
-        // var type_column = "'TypeText'";
-        // var tempWhereClause = [];
-        // if ( $("#cbType1").is(':checked')) tempWhereClause.push('District');
-        // if ( $("#cbType2").is(':checked')) tempWhereClause.push('Interdistrict Magnet');
-        // if ( $("#cbType3").is(':checked')) tempWhereClause.push('Charter');
-        // if ( $("#cbType4").is(':checked')) tempWhereClause.push('Technical');
-        // if ( $("#cbType5").is(':checked')) tempWhereClause.push('Other');
-        // self.whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
+        var type_column = "'Main Technology'";
+        var tempWhereClause = [];
+        if ( $("#cbType1").is(':checked')) tempWhereClause.push('DSF');
+        if ( $("#cbType2").is(':checked')) tempWhereClause.push('Openings');
+        if ( $("#cbType3").is(':checked')) tempWhereClause.push('Shading System');
+        if ( $("#cbType4").is(':checked')) tempWhereClause.push('Smart Material');
+        if ( $("#cbType5").is(':checked')) tempWhereClause.push('Kinetic Building');
+        self.whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
 
         //-----end of custom filters-----
 
